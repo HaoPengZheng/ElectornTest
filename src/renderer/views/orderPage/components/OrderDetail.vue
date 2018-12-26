75c91c9047f0ad14885ea15b385c4795ffecdf93
= <haopengzh@gmail.com>
2018-12-26 15:57:35 +0800

    房间细节加标签

--- src/renderer/views/orderPage/components/OrderDetail.vue
+++ src/renderer/views/orderPage/components/OrderDetail.vue
@@ -80,10 +80,21 @@
               class="td_bs_remark"
               colspan="5"
             ><span class="remark_txt"></span>
-              <a
-                href="javascript:void(0)"
-                onclick="editOrderRemark()"
-              >添加</a>
+              <el-tag
+                v-for="(tag,index) in remarkList"
+                :key="tag"
+                :color="colorList[index]" class="my-tag"
+                :style="{'color':oppositeColor(colorList[index])}"
+                closable
+              >
+                {{tag}}
+              </el-tag>
+              <el-button
+                type="text"
+                @click="showBackendRemarkDialog"
+              >
+                添加
+              </el-button>
             </td>
           </tr>
           <tr class="tr_pay_remark">
@@ -104,14 +115,113 @@
         </tbody>
       </table>
     </div>
+    <div id="backendRemarkDialog">
+      <el-dialog
+        title="后台备注"
+        :visible.sync="backendRemarkDialogVisible"
+        append-to-body
+        width="30%"
+      >
+        <div>
+          <div>
+            <el-checkbox-group
+              v-model="remarkList"
+              size="small"
+            >
+              <el-checkbox
+                :label="option"
+                v-for="(option,index) in remarkOptions"
+                :key="option"
+                border
+                :class="{'mg-left-10':index===0}"
+              ></el-checkbox>
+            </el-checkbox-group>
+          </div>
+          <div class="mg-left-10">
+            <el-input
+              type="textarea"
+              placeholder="填写备注内容"
+            ></el-input>
+          </div>
+          <div class="remark-footer">
+            <el-button
+              type="danger"
+              size="small"
+            >保存</el-button>
+          </div>
+        </div>
+      </el-dialog>
+    </div>
   </div>
 </template>
 <script>
 export default {
-
+  data () {
+    return {
+      backendRemarkDialogVisible: false,
+      remarkList: [],
+      remarkOptions: ['散客', 'L类保密', '尊尚', '加1童', '大床', '非吸烟', '送单车', '送单车及千色', '双床', '相连房', '高楼层', '送千色', '时光邮驿'],
+      colorList: []
+    }
+  },
+  watch: {
+    remarkList (newList) {
+      if (this.colorList.length < newList.length) {
+        this.colorList.push(this.color16())
+      }
+    }
+  },
+  methods: {
+    showBackendRemarkDialog () {
+      this.backendRemarkDialogVisible = true
+    },
+    color16 () {
+      var r = Math.floor(Math.random() * 250)
+      var g = Math.floor(Math.random() * 250)
+      var b = Math.floor(Math.random() * 250)
+      var color = '#' + r.toString(16) + g.toString(16) + b.toString(16)
+      if (color.length < 7) {
+        color += Math.floor(Math.random() * 10)
+      }
+      console.log(color)
+      return color
+    },
+    oppositeColor (color) {
+      let whiteColor = '#fff'
+      let blackColor = '#000'
+      let r = parseInt(color.substr(1, 2), 16)
+      let g = parseInt(color.substr(3, 2), 16)
+      let b = parseInt(color.substr(5, 2), 16)
+      let count = 0
+      if (r > 150) {
+        count++
+      }
+      if (g > 150) {
+        count++
+      }
+      if (b > 150) {
+        count++
+      }
+      if (count > 2) {
+        return blackColor
+      }
+      return whiteColor
+    }
+  }
 }
 </script>
+<style lang="scss">
+.my-tag{
+  .el-icon-close{
+    color: inherit;
+  }
+}
+</style>
+
 <style lang="scss" scoped>
+.mg-left-10 {
+  margin-left: 10px;
+}
 .t {
   width: 100%;
   border-collapse: collapse;
@@ -164,6 +274,9 @@ export default {
     float: left;
   }
 }
+.my-tag{
+  margin-right: 10px;
+}
 .table_goods {
   margin-bottom: 0 !important;
 }
@@ -174,9 +287,23 @@ export default {
       border-top: 0;
     }
   }
+  .td_title {
+    min-width: 100px;
+    width: 110px;
+    background: #ffffff;
+    text-align: left;
+  }
 }
-.footer{
+.td_title {
+  min-width: 100px;
+  font-weight: 700;
+}
+.remark-footer,
+.footer {
   display: flex;
   justify-content: flex-end;
+}
+.remark-footer {
+  margin-top: 20px;
 }
 </style>
